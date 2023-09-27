d442df30531a8afd60ec9a3ac22a9147903b6946
robpodosek <rpodosek@gmail.com>
2023-09-27 11:34:31 -0700

    Initial Spring Initializr Commit.

---
+++ src/main/java/com/charter/rewardprogram/RewardProgramApiApplication.java
@@ -0,0 +1,13 @@
+package com.charter.rewardprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardProgramApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardProgramApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charter/rewardprogram/RewardProgramApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.rewardprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardProgramApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
